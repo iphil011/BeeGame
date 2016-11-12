c9880692a344602644afd5d996efbd1ef0300c8b
Prabhat Verma <Prabhat Verma>
2016-11-12 14:53:52 +0000

    Bee Basic Movement

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+
+	private Rigidbody rb;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		float moveVertical2 = Input.GetAxis ("Vertical2");
+
+		Vector3 movement = new Vector3 (moveHorizontal, moveVertical, moveVertical2);
+
+		rb.AddForce (movement * speed);
+	}
+}
